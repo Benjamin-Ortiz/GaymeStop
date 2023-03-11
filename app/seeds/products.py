1ec1e02f119ff1ea7d07351f6e26ddfb191dc265
Ben <ortizbenjaminj@gmail.com>
2023-03-11 18:29:46 -0500

    added sims4

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -20,12 +20,19 @@ def seed_products():
         glitter_factor="Character Customization goes above and beyond and doesnt force default confromity between sexes, everything is customizable, maybe too much is customizable. Nothing holds you back from playing the game your way and there's many storyline npcs you can develope relations with, as expected in the year 2077",
         product_image='https://onlyjamsbucket.s3.amazonaws.com/gaymeStop/gayStop-images/cyberpunk64.jpg'
         )
+    sims4 = Product(
+        user_id=1,
+        title='The Sims 4',
+        price= 19,
+        description='DISSOCIATION TIME BAYBEEEEEE',
+        glitter_factor="It's the Sims, nuff said.You could see some first hints at LGBTQ+ households in some of The Sims 3 Worlds, however they wouldn’t be added in until the game’s ending cycle. The real story begins with The Sims 4, where more and more proper LGBTQ+ representation is shown.",
+        product_image='https://simscommunity.info/wp-content/uploads/2019/07/boxart.jpg'
+        )
 
 
 
 
-
-    db.session.add_all([cyberpunk, cyberpunk2])
+    db.session.add_all([cyberpunk, cyberpunk2, sims4])
     db.session.commit()
 
 
