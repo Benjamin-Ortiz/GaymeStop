7d1017f252bbc8c1ad887e4fbb98735790635642
Ben <ortizbenjaminj@gmail.com>
2023-03-31 16:06:32 -0400

    search returns dict of titles, descripts, glitter factors

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -20,24 +20,23 @@ def validation_errors_to_error_messages(validation_errors):
 
 @search_routes.route("/products/<query>", methods=["GET"])
 def get_results(query):
-    # if (current_user.is_authenticated):
-    #     desired_search = Search(search=query, user_id=current_user.id)
-    #     db.session.add(desired_search)
-    #     db.session.commit()
 
-    # title_search = Product.query.filter(Product.title.contains('Cyber'))
-    title_search = Product.query.filter(Product.title.contains(query)).all()
+    results = {}
 
-
+    title_search = Product.query.filter(Product.title.contains(query))
     description_search = Product.query.filter(Product.description.contains(query))
     glitter_factor_search = Product.query.filter(Product.glitter_factor.contains(query))
 
     if title_search:
     #    results.append(title_search)
-        print([product.to_dict() for product in title_search], " title searchhhhhh " *9)
+        #  print([product.to_dict() for product in title_search], " title searchhhhhh " *9)
+        title_res = {
+             'Matching Titles' : [product.title for product in title_search],
+             'Matching Descriptions' : [product.title for product in description_search],
+             'Matching Glitter Factors' : [product.title for product in glitter_factor_search]
+             }
+        # print(type(title_res), "TYPE" * 5)
+        return title_res
+    else : return {"message": "No results found"}
 
-        # return {title.to_dict() for title in title_search}
-        return {'Search Results' : [product.title for product in title_search]}
-    # return {product.to_dict() for product in title_search}
-    else:
-        return {"message": "No results found"}
+    return {"message": "No results found"}
